1a2f852c8f983308f05702284a9dd1edb9030497
unknown <m.lazarte.rodriguez@gmail.com>
2020-07-12 14:13:04 +0200

    First commit

--- server.lua
+++ server.lua
@@ -1,31 +1,33 @@
 --Private Messages
 TriggerEvent(
     "redem:addCommand",
-    "pm",
+    "doc",
     function(source, args, user)
-        -- Es posible que haya que cambiar args[1] por args[2]
         if GetPlayerName(tonumber(args[1])) then
             local player = tonumber(args[1])
-            --table.remove(args, 2)
-            --table.remove(args, 1)
-
             TriggerEvent(
                 "redem:getPlayerFromId",
                 player,
-                function(target)
-                    TriggerClientEvent(
-                        "chatMessage",
+                function(sourceRoleplayer)
+                    TriggerEvent(
+                        "redem:getPlayerFromId",
                         player,
-                        "PM",
-                        {214, 214, 214},
-                        "^2 De ^5" .. GetPlayerName(source) .. " [" .. source .. "]: ^7" .. table.concat(args, " ")
-                    )
-                    TriggerClientEvent(
-                        "chatMessage",
-                        source,
-                        "PM",
-                        {214, 214, 214},
-                        "^3 Enviado a ^5" .. GetPlayerName(player) .. ": ^7" .. table.concat(args, " ")
+                        function(playerRoleplayer)
+                            TriggerClientEvent(
+                                "chatMessage",
+                                player,
+                                "Documentación",
+                                {214, 214, 214},
+                                "^2 De ^5" .. GetPlayerName(source) .. " [" .. source .. "]: " .. sourceRoleplayer.getFirstname() .. " " .. sourceRoleplayer.getLastname()
+                            )
+                            TriggerClientEvent(
+                                "chatMessage",
+                                source,
+                                "Documentación",
+                                {214, 214, 214},
+                                "^3 Enviado a ^5" .. GetPlayerName(player) .. "."
+                            )
+                        end
                     )
                 end
             )
